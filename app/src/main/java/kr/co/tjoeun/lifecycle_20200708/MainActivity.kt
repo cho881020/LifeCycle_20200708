7b7fa6e19ff8c9bf3867a27c71b94813af60af1d
조경진 <kj_cho@idealidea.co.kr>
2020-07-09 19:42:19 +0900

    onResume테스트

--- app/src/main/java/kr/co/tjoeun/lifecycle_20200708/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/lifecycle_20200708/MainActivity.kt
@@ -1,11 +1,28 @@
 package kr.co.tjoeun.lifecycle_20200708
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("메인화면", "onCreate 실행")
+
+        goToOtherBtn.setOnClickListener {
+            val myIntent = Intent(this, OtherActivity::class.java)
+            startActivity(myIntent)
+        }
+
     }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("메인화면", "onResume 실행")
+    }
+
 }
